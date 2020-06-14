865a6893dd64b49b7940a1692cf3dd6ba07e7a59
Jef Dewitt <jef.dewitt@twinspires.com>
2020-06-14 12:51:38 -0400

    Completed bst project.

---
+++ cmake-build-debug/CMakeFiles/Lab6_2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab6_2.dir/BinarySearchTree.cpp.o"
+  "CMakeFiles/Lab6_2.dir/CSVparser.cpp.o"
+  "Lab6_2"
+  "Lab6_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab6_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
